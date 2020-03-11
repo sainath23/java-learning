a137f582765a5c8a4f54aaf659a04795ef900758
sainath <saiparkar4@gmail.com>
2020-03-12 02:35:37 +0530

    learnt phaser

--- src/com/sainath/concurrentcollection/ConcurrentHashMapDemo.java
+++ src/com/sainath/concurrentcollection/ConcurrentHashMapDemo.java
@@ -1,12 +1,13 @@
 package com.sainath.concurrentcollection;
 
+import java.util.Objects;
 import java.util.concurrent.ConcurrentHashMap;
 import java.util.concurrent.ConcurrentMap;
 
 public class ConcurrentHashMapDemo {
 
     public static void main(String[] args) {
-        ConcurrentMap<Integer, String> map = new ConcurrentHashMap<>();
+        ConcurrentMap<Integer, String> map = new ConcurrentHashMap<>(32, 0.50f, 42);
 
         System.out.println(map.putIfAbsent(1, "Sainath"));
         System.out.println(map.putIfAbsent(1, "Parkar"));
@@ -14,5 +15,6 @@ public class ConcurrentHashMapDemo {
         System.out.println(map.remove(1, "ABC"));
         System.out.println(map.replace(1, "Parkar", "Sainath"));
         //System.out.println(map.put(2, null)); // NPE
+        System.out.println(Objects.hash(null));
     }
 }
